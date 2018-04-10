2ea5bc5c496de52941a98f5b048aed8d19161862
wynand <wtheron2@yahoo.com>
2018-04-10 19:07:59 +0200

    made website responsive

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 $(document).ready(function() {
-  alert("please note that our website is not yet mobile friendly, we are working on it!");
+
+//dropdown burger menu for mobile
+$("#burger-nav").on("click", function() {
+  $("header nav ul").toggleClass("open");
+})
+
+
+//to make dropdown menu dissapear when a nav item is clicked
+$("header nav li a").on("click", function() {
+  if($("header nav ul").hasClass("open")) {
+    $("header nav ul").removeClass("open");
+  }
+})
+
+
+//slideshow
 var slideChanger = document.getElementById('slide');
 
 function changeColor() {
   slideChanger.style.backgroundImage = "url('pics/facts/"+randomNum()+".jpg')";
-  slideChanger.fadeIn(200);
 }
 
 setInterval(changeColor,10000);
